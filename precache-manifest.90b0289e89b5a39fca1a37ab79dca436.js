f1573d0e1b0636027be767bd831369d32dcac364
MiroslavMashivskyi <62468496+MiroslavMashivskyi@users.noreply.github.com>
2020-07-19 21:52:19 +0300

    Updates

---
+++ precache-manifest.90b0289e89b5a39fca1a37ab79dca436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1672742ed03135fbd467e3f04f6c88bb",
+    "url": "/my-money/index.html"
+  },
+  {
+    "revision": "19b6a56bbc9b04002c3e",
+    "url": "/my-money/static/css/main.98736f35.chunk.css"
+  },
+  {
+    "revision": "972386ef14604e7e4595",
+    "url": "/my-money/static/js/2.1fc4cd0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-money/static/js/2.1fc4cd0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b6a56bbc9b04002c3e",
+    "url": "/my-money/static/js/main.7f2cbc1a.chunk.js"
+  },
+  {
+    "revision": "bf7b723be9ed6ae69866",
+    "url": "/my-money/static/js/runtime-main.4884bd8a.js"
+  },
+  {
+    "revision": "c77cc1fff286191839dc05090582b415",
+    "url": "/my-money/static/media/GrenzeGotisch-Medium.c77cc1ff.ttf"
+  }
+]);
